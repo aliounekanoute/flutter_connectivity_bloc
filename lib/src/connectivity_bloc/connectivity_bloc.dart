b5acfdb3d915484996ce5063c5b3a6b00aa31361
Alioune Kanoute <aliounekanoute@gmail.com>
2023-05-21 14:38:26 +0000

    chore: dependencies upgrades and documentation

--- lib/src/connectivity_bloc/connectivity_bloc.dart
+++ lib/src/connectivity_bloc/connectivity_bloc.dart
@@ -7,27 +7,39 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 part 'connectivity_event.dart';
 part 'connectivity_state.dart';
 
+/// A BLoC (Business Logic Component) responsible for managing connectivity events and states.
+/// It extends the `Bloc` class, with `ConnectivityEvent` as the event type and `ConnectivityState` as the state type.
 class ConnectivityBloc extends Bloc<ConnectivityEvent, ConnectivityState> {
   StreamSubscription? subscription;
 
+  /// Constructs a new instance of the `ConnectivityBloc`.
+  /// It initializes the state to `ConnectivityInitialState`.
   ConnectivityBloc() : super(ConnectivityInitialState()) {
+    // Handle the 'OnConnectivityEvent' by emitting a `ConnectivitySuccessState`.
     on<OnConnectivityEvent>((event, emit) => emit(ConnectivitySuccessState()));
+
+    // Handle the 'OnNotConnectivityEvent' by emitting a `ConnectivityFailureState`.
     on<OnNotConnectivityEvent>((event, emit) => emit(ConnectivityFailureState()));
 
+    // Subscribe to connectivity changes using the `Connectivity` plugin.
     subscription = Connectivity()
         .onConnectivityChanged
         .listen((ConnectivityResult result) {
       if (result == ConnectivityResult.mobile ||
           result == ConnectivityResult.wifi) {
+        // If the connectivity result is mobile or wifi, add an 'OnConnectivityEvent'.
         add(OnConnectivityEvent());
       } else {
+        // Otherwise, add an 'OnNotConnectivityEvent'.
         add(OnNotConnectivityEvent());
       }
     });
   }
+  
+  /// Closes the BLoC and performs necessary cleanup.
   @override
   Future<void> close() {
-    subscription?.cancel();
-    return super.close();
+    subscription?.cancel(); // Cancel the subscription to stop listening for connectivity changes.
+    return super.close(); // Close the BLoC and release any resources.
   }
 }
--- lib/src/connectivity_bloc/connectivity_event.dart
+++ lib/src/connectivity_bloc/connectivity_event.dart
@@ -1,8 +1,12 @@
 part of 'connectivity_bloc.dart';
 
+/// The base abstract class for different connectivity events.
+/// It is marked as immutable, indicating that its subclasses should also be immutable.
 @immutable
 abstract class ConnectivityEvent {}
 
-class OnConnectivityEvent extends ConnectivityEvent{}
+/// Represents an event when connectivity is established.
+class OnConnectivityEvent extends ConnectivityEvent {}
 
-class OnNotConnectivityEvent extends ConnectivityEvent{}
+/// Represents an event when connectivity is lost.
+class OnNotConnectivityEvent extends ConnectivityEvent {}
